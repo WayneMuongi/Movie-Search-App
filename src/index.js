8d2cfa2ba3d96cc2d62c9a17b7cc52bbe8967ca0
WayneMuongi <waynemuongi77@gmail.com>
2025-06-29 15:27:59 +0300

    assingning of variables and adding api keys that will fetch the data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+   const API_KEY = '5dde9039';
+const API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const moviesGrid = document.getElementById('moviesGrid');
+const favoritesGrid = document.getElementById('favoritesGrid');
+
+let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+});
+    
